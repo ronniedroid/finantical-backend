fc3aa889dd2858be8dd43f9f3158649e5e803516
ronnie nissan <ronni.n.yousif@gmail.com>
2021-07-05 12:11:19 +0300

    added basic project structure

---
+++ server.js
@@ -0,0 +1,26 @@
+const fastify = require("fastify")({logger: true});
+
+const PORT = 5000
+
+fastify.register(require("fastify-swagger"), {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger: {
+    info: {title: 'finantical-backend'}
+  }
+})
+
+fastify.get('/message', (reg, reply) => {
+  reply.send({test: 'we are ok'})
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT)
+  } catch (error) {
+    fastify.log.error(error)
+    process.exit(1)
+  }
+}
+
+start()
